f44af4c47251399437643382d540ca2ebe585684
Kremerin <110621622+kremerin@users.noreply.github.com>
2022-08-18 19:53:06 +0200

    Vers1

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+function intro() {
+    console.log("Hello World");
+}
+intro();
+
+let intro2 = ()  => {
+    console.log("Hello World 2");
+};
+intro2();
+
+const intro3 = () => console.log("Hello World 3");
+intro3();
